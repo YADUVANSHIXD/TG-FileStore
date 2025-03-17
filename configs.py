21172a5bd709a4c1f9a7681200d58f0ccbb514af
Pradeep Yadav <136789010+YADUVANSHIXD@users.noreply.github.com>
2025-03-17 10:59:32 +0530

    Add files via upload

--- configs.py
+++ configs.py
@@ -1,24 +1,25 @@
-# (c) @PredatorHackerzZ || @TeleRoidGroup
+# (c) @PredatorHackerzZ || @YaduvanshiXD
 
 import os
 
 class Config(object):
-	API_ID = int(os.environ.get("API_ID", "0"))
-	API_HASH = os.environ.get("API_HASH")
-	BOT_TOKEN = os.environ.get("BOT_TOKEN")
-	BOT_USERNAME = os.environ.get("BOT_USERNAME")
-	DB_CHANNEL = int(os.environ.get("DB_CHANNEL", "-100"))
-	SHORTLINK_URL = os.environ.get('SHORTLINK_URL')
-	SHORTLINK_API = os.environ.get('SHORTLINK_API')
-	BOT_OWNER = int(os.environ.get("BOT_OWNER", "1445283714"))
-	DATABASE_URL = os.environ.get("DATABASE_URL")
-	UPDATES_CHANNEL = os.environ.get("UPDATES_CHANNEL", "")
-	LOG_CHANNEL = os.environ.get("LOG_CHANNEL", None)
+	API_ID = int(os.environ.get("API_ID", "29728878"))
+	API_HASH = os.environ.get("API_HASH", "a961168f7807061e77e1fb39c3f6ef71")
+	BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
+	BOT_USERNAME = os.environ.get("BOT_USERNAME", "Gdhffsfjbot")
+	DB_CHANNEL = int(os.environ.get("DB_CHANNEL", "-1002480353700"))
+	SHORTLINK_URL = os.environ.get('SHORTLINK_URL', "softurl.in")
+	SHORTLINK_API = os.environ.get('SHORTLINK_API', "cb6243bee96a21ea399e214f1484a7de4652edc9")
+	BOT_OWNER = int(os.environ.get("BOT_OWNER", "7033385522"))
+	DATABASE_URL = os.environ.get("DATABASE_URL", "mongodb+srv://sushankm16:411WAfPYKWyqPIDD@cluster0.sngp9pz.mongodb.net/?retryWrites=true&w=majority")
+	UPDATES_CHANNEL = os.environ.get("UPDATES_CHANNEL", "-1002190750585")
+	LOG_CHANNEL = os.environ.get("LOG_CHANNEL", "-1002168042476")
 	BANNED_USERS = set(int(x) for x in os.environ.get("BANNED_USERS", "1234567890").split())
 	FORWARD_AS_COPY = bool(os.environ.get("FORWARD_AS_COPY", True))
-	BROADCAST_AS_COPY = bool(os.environ.get("BROADCAST_AS_COPY", False))
+	BROADCAST_AS_COPY = bool(os.environ.get("BROADCAST_AS_COPY", True))
 	BANNED_CHAT_IDS = list(set(int(x) for x in os.environ.get("BANNED_CHAT_IDS", "-1001362659779 -1001255795497").split()))
 	OTHER_USERS_CAN_SAVE_FILE = [int(id) for id in os.environ.get("OTHER_USERS_CAN_SAVE_FILE", "").split(",") if id.strip()]
+	#OTHER_USERS_CAN_SAVE_FILE = bool(os.environ.get("OTHER_USERS_CAN_SAVE_FILE", True))
 	ABOUT_BOT_TEXT = f"""
 This is a Permanent FileStore Bot. 
 Send Me any Media or File. I can Work In Channel too. Add Me to Channel with Edit Permission, I will add save Uploaded File in Channel and Share a Shareable Link. 
@@ -42,7 +43,7 @@ Send Me any Media or File. I can Work In Channel too. Add Me to Channel with Edi
 ╰──────[ 😎 ]───────────⍟
 """
 	ABOUT_DEV_TEXT = f"""
-🧑🏻‍💻 **𝗗𝗲𝘃𝗲𝗹𝗼𝗽𝗲𝗿:** [@PredatorHackerzZ](https://github.com/PredatorHackerzZ)
+🧑🏻‍💻 **𝗗𝗲𝘃𝗲𝗹𝗼𝗽𝗲𝗿:** [@YaduvanshiXD](https://github.com/YADUVANSHIXD)
  
  I am Super noob Please Support My Hard Work.
 
